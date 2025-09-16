e86a9c7d55d278edd02d6138e5ad68b13befbba6
DepressiaGitHub <59769268+DepressiaGitHub@users.noreply.github.com>
2025-09-16 04:30:00 +0300

    Добавляет алерты.

--- build/js/common.js
+++ build/js/common.js
@@ -5,30 +5,38 @@ const debounce = window.Corners5ProjectLayout.debounce;
 const requestForm = document.querySelector('.request__form');
 if (requestForm) {
   requestForm.addEventListener('bouncerFormValid', debounce((evt) => {
-    const fd = new FormData(evt.target);
 
-    return $.ajax({
-      type: 'POST',
-      data: fd,
-      url: '/local/ajax/formHelp.php',
-      contentType: false,
-      processData: false,
-      cache: false,
-      async: false,
-      dataType: 'json',
-      success(data) {
-        // var msg = JSON.parse(data);
-        if (data.success == true) {
-          ym(84981199, 'reachGoal', 'help_in_choise');
-
-          window.Corners5ProjectLayout.summonAlert({
-            template: '#alert--blue',
-            text: data.text,
-          });
-          
-          requestForm.reset();
-        }
-      },
+    window.Corners5ProjectLayout.summonAlert({
+      template: '#alert--blue',
+      text: 'Логика для формы на главной "Перезвоните мне"',
     });
+
+    requestForm.reset();
+
+    // const fd = new FormData(evt.target);
+
+    // return $.ajax({
+    //   type: 'POST',
+    //   data: fd,
+    //   url: '/local/ajax/formHelp.php',
+    //   contentType: false,
+    //   processData: false,
+    //   cache: false,
+    //   async: false,
+    //   dataType: 'json',
+    //   success(data) {
+    //     // var msg = JSON.parse(data);
+    //     if (data.success == true) {
+    //       ym(84981199, 'reachGoal', 'help_in_choise');
+
+    //       window.Corners5ProjectLayout.summonAlert({
+    //         template: '#alert--blue',
+    //         text: data.text,
+    //       });
+
+    //       requestForm.reset();
+    //     }
+    //   },
+    // });
   }, 500));
 }
